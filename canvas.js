817ea38b46868aaae32cef273e1e291d1116afdb
Heitor Felix <heitorcfelix@gmail.com>
2018-06-02 10:11:49 -0300

    creating points

--- canvas.js
+++ canvas.js
@@ -1,5 +1,85 @@
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
+function resizeCanvas(width, height) {
+    canvas.width = width;
+    canvas.height = height;
+}
 
-ctx.font = "30px Arial";
-ctx.fillText("Hello World", 10, 50);
+function resizeToFit() {
+    var width = parseFloat(window.getComputedStyle(canvas).width);
+    var height = parseFloat(window.getComputedStyle(canvas).height);
+    resizeCanvas(width, height);
+}
+
+function isInCircle(click) {
+    var v;
+    for (i = 0; i < points.length; i++) {
+        v = {
+            x: points[i].x - click.x,
+            y: points[i].y - click.y
+        };
+
+        if (Math.sqrt(v.x * v.x + v.y * v.y) <= point.radius) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+function draw_point() {
+    ctx.beginPath();
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = '5';
+    ctx.fillStyle = 'red';
+    ctx.arc(point.x, point.y, point.radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+
+function draw_points() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (i = 0; i < points.length; i++) {
+        point.x = points[i].x;
+        point.y = points[i].y;
+        draw_point();
+    }
+}
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var point = {
+    x: 0,
+    y: 0,
+    radius: 5
+};
+var move = -1;
+var points = []
+
+resizeToFit();
+
+canvas.addEventListener('mousedown', function(e) {
+    move = isInCircle({
+        x: e.offsetX,
+        y: e.offsetY
+    });
+
+    if (move < 0) {
+        point.x = e.offsetX;
+        point.y = e.offsetY;
+        points.push({x: point.x, y: point.y});
+        draw_points();
+    }
+});
+
+canvas.addEventListener('mousemove', function(e) {
+    if (move >= 0) {
+        points[move] = {
+            x: e.offsetX,
+            y: e. offsetY
+        }
+        draw_points();
+    }
+});
+
+canvas.addEventListener('mouseup', function(e) {
+    move = -1;
+});
